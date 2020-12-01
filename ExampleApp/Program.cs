a2af9403704330e50914f70917640b421043613a
TatoExp <mwmatthew10@gmail.com>
2020-12-01 20:42:05 +0000

    idk

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -8,39 +8,48 @@ namespace ExampleApp
 	{
 		static void Main(string[] args)
 		{
-			Express server = new Express();
-
-			server.GET("/", (req, res) =>
+			try
 			{
-				res.Status(200).Json(new { Id = 123, Username = "Test Username" });
-			});
+				Express server = new Express();
 
-			server.GET("/helloworld", (req, res) =>
+				server.GET("/", (req, res) =>
+				{
+					res.Status(200).Json(new { Id = 123, Username = "Test Username" });
+				});
+
+				server.GET("/helloworld", (req, res) =>
+				{
+					res.Status(200).Send("Hello, World!");
+				});
+
+				server.POST("/TestPost", (req, res) =>
+				{
+					res.Status(200).Json(new { Id = 300 });
+				});
+
+				server.PATCH("/TestPatch", (req, res) =>
+				{
+					res.Status(200).Json(new { Id = 301 });
+				});
+
+				server.DELETE("/TestDelete", (req, res) =>
+				{
+					res.Status(200).Json(new { Id = 302 });
+				});
+
+				server.DELETE("/TestPut", (req, res) =>
+				{
+					res.Status(200).Json(new { Id = 303 });
+				});
+
+				server.Listen();
+			}
+			catch (Exception ex)
 			{
-				res.Status(200).Send("Hello, World!");
-			});
-
-			server.POST("/TestPost", (req, res) =>
-			{
-				res.Status(200).Json(new { Id = 300 });
-			});
-
-			server.PATCH("/TestPatch", (req, res) =>
-			{
-				res.Status(200).Json(new { Id = 301 });
-			});
-
-			server.DELETE("/TestDelete", (req, res) =>
-			{
-				res.Status(200).Json(new { Id = 302 });
-			});
-
-			server.DELETE("/TestPut", (req, res) =>
-			{
-				res.Status(200).Json(new { Id = 303 });
-			});
-
-			server.Listen();
+				Console.WriteLine("Error occured starting server");
+				Console.WriteLine(ex.ToString());
+			}
+			Console.WriteLine("Closing...");
 		}
 	}
 }
