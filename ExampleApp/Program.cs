3c137fda6e3f2a2b985a98b8ca66bdf25f1add34
TatoExp <mwmatthew10@gmail.com>
2020-12-01 20:37:05 +0000

    addressed more qual issues maybe?

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ExpressSharp;
+namespace ExampleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Express server = new Express();
+
+			server.GET("/", (req, res) =>
+			{
+				res.Status(200).Json(new { Id = 123, Username = "Test Username" });
+			});
+
+			server.GET("/helloworld", (req, res) =>
+			{
+				res.Status(200).Send("Hello, World!");
+			});
+
+			server.POST("/TestPost", (req, res) =>
+			{
+				res.Status(200).Json(new { Id = 300 });
+			});
+
+			server.PATCH("/TestPatch", (req, res) =>
+			{
+				res.Status(200).Json(new { Id = 301 });
+			});
+
+			server.DELETE("/TestDelete", (req, res) =>
+			{
+				res.Status(200).Json(new { Id = 302 });
+			});
+
+			server.DELETE("/TestPut", (req, res) =>
+			{
+				res.Status(200).Json(new { Id = 303 });
+			});
+
+			server.Listen();
+		}
+	}
+}
